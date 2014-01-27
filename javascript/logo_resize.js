577f4234c9cc02c49f916951a2a30a5c725ff4d6
Tobias Pfeiffer <tobias.pfeiffer@student.hpi.uni-potsdam.de>
2014-01-27 21:34:06 +0100

    Name section profile-links as bastis adblocker blocks them otherwise

--- javascript/logo_resize.js
+++ javascript/logo_resize.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   var FONT_MODIFIER = 0.9;
-  var fontSize = (FONT_MODIFIER * parseInt($("#social-profiles .logo-space").width()))+"px";
+  var fontSize = (FONT_MODIFIER * parseInt($("#profile-links .logo-space").width()))+"px";
 //  console.log(fontSize);
-  $("#social-profiles .my-icon").css('font-size', fontSize);
+  $("#profile-links .my-icon").css('font-size', fontSize);
 });
