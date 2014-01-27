e4473a8447da93e77165b32717378ca21975e3f9
Tobias Pfeiffer <tobias.pfeiffer@student.hpi.uni-potsdam.de>
2014-01-27 21:41:46 +0100

    resize fonts on windows resize

--- javascript/logo_resize.js
+++ javascript/logo_resize.js
@@ -1,6 +1,12 @@
 $(document).ready(function() {
   var FONT_MODIFIER = 0.9;
-  var fontSize = (FONT_MODIFIER * parseInt($("#profile-links .logo-space").width()))+"px";
-//  console.log(fontSize);
-  $("#profile-links .my-icon").css('font-size', fontSize);
+
+  var resizeLogos = function() {
+    var fontSize = (FONT_MODIFIER * parseInt($("#profile-links .logo-space").width()))+"px";
+    $("#profile-links .my-icon").css('font-size', fontSize);
+  };
+
+  resizeLogos();
+
+  $(window).resize(resizeLogos);
 });
