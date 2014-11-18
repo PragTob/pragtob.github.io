ed1a89ccabf73a5e595e0392fa75d68e7c16771e
Tobias Pfeiffer <pragtob@gmail.com>
2014-11-18 15:08:27 +0100

    Add PDF and add a link to the PDF

--- generate_pdf.sh
+++ generate_pdf.sh
@@ -1,1 +1,1 @@
-wkhtmltopdf --footer-left "Tobias Pfeiffer" --footer-right "[page]/[toPage]" _site/resume_print.html ~/resume.pdf
+wkhtmltopdf --footer-left "Tobias Pfeiffer" --footer-right "[page]/[toPage]" _site/resume_print.html tobias_pfeiffer_resume.pdf
